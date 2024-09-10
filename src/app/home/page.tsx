3cf184ca0df3b2169e598d51b42f2e16fc5db136
Shahbaz <shahbaz.bashir.nbt@gmail.com>
2024-09-10 13:55:11 +0500

    home page done

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>Home
+        
+        <h1>Shahbaz</h1>
+        <h2>Shahbaz</h2>
+        
+         </div>
+    
+  )
+}
